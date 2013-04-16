099b17411bd0e926fad501f56f6ca5b73905395f
tuoyo <eyituoyo@yahoo.com>
2013-04-16 17:27:55 -0400

    Jackson/JsonIgnore test - an additional annotation for getter

--- src/test/java/misc/stack/json/HasMoney.java
+++ src/test/java/misc/stack/json/HasMoney.java
@@ -3,10 +3,13 @@ package misc.stack.json;
 import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface HasMoney {
     @JsonProperty
+    @JsonInclude(Include.NON_NULL)
     BigDecimal getMoney();
 
     @JsonIgnore
